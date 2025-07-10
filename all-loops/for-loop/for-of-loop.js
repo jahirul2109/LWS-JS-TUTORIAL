e17fb42d54397c34413d88b8ce89bee50d1169aa
Jahirul Islam <jisjis115@gmail.com>
2025-07-10 16:55:01 +0600

    Renamed Folder

---
+++ all-loops/for-loop/for-of-loop.js
@@ -0,0 +1,16 @@
+
+// When value is array then use for-of loop 
+
+let person = "Jahirul";
+for (let a of person){
+    console.log(person[1]);
+    console.log(a); // output: j a h i r u l for of print value index doesn't matter
+}
+
+
+// For-in-loop
+// let person = "Jahirul";
+// for (let a in person){
+//     console.log(person[1]);
+//     console.log(a); // output: 0,1,2,3,4,5,6 this is print index value 
+// }
