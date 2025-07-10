4e47ab66f17ef565c149adb06148cf7f391d56ce
Jahirul Islam <jisjis115@gmail.com>
2025-07-10 16:36:46 +0600

    added while loop

---
+++ all-loops/for-loop/while-loop.js
@@ -0,0 +1,31 @@
+let number = 0 ;
+let name = "";  // Initialization above the loop 
+
+while (number < 10) {  // Only condition involv in parameter
+    name += " jahirul " + number;
+    console.log(`Name: ${name} \n Roll : ${number}`); // Loop body 
+    console.log(number);
+    console.log(name);
+    console.log(number);
+    number++;   // Increment & decrement define in last step under the loop 
+    
+}
+ console.log(name);
+ console.log(number);
+ 
+ console.log(`Name: ${name} \n Roll : ${number}`); 
+
+
+// Do While Loop 
+
+do {
+    name += " jahirul " + number;
+    console.log(`Name: ${name} \n Roll : ${number}`); // Loop body 
+    console.log(number);
+    console.log(name);
+    console.log(number);
+    number++;   // Increment & decrement define in last step under the loop 
+    
+} while (number > 10); // Condition Check
+
+ 
